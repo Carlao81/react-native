93cf76497073bdfb7ef858c1325457c751971a87
nandomonarca <160517220+Carlao81@users.noreply.github.com>
2024-05-24 10:22:03 -0300

    continuação_do_react
    continuação do baguio

---
+++ blaze/src/screens/index.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import { View, Text} from 'react-native';
+import { styles } from "./styles";
+
+export function SignIn() {
+  return( 
+    <View style ={{flex: 1, backgroundColor: 'red', alignItems: 'center', justifyContent: 'center'}}>
+     <Text>I am your father</Text>
+    </View>
+  );
+}
+
+
+
+
+
